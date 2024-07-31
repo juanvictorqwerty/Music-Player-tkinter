85732077f90531681aee13906c5a287ca2b757a2
Juan <juanvictorqwerty@gmail.com>
2024-07-31 02:41:07 +0100

    button amelioration

--- Music_Player.py
+++ Music_Player.py
@@ -28,9 +28,9 @@ pause_button = Button (control_frame,image=pause_button_image,borderwidth=0,widt
 next_button = Button (control_frame,image=next_button_image,borderwidth=0,width=0,height=100)
 previous_button = Button (control_frame,image=previous_button_image,borderwidth=0,width=200,height=100)
 
-play_button.grid(row=0, column=1)
-pause_button.grid(row=0, column=2)
-next_button.grid(row=0, column=3)
-previous_button.grid(row=0, column=0)
+play_button.grid(row=0, column=1, padx=1,pady=2)
+pause_button.grid(row=0, column=2, padx=1,pady=2)
+next_button.grid(row=0, column=3, padx=1,pady=2)
+previous_button.grid(row=0, column=0 , padx=2,pady=2)
 
 root.mainloop() #run the code
